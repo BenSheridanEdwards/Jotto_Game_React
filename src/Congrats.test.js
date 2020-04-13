53cee5face3773f482e1fe411f916a20ae6ebfc5
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-13 14:19:58 +0100

    chore: refactor out checkProps test helper to testUtils

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
-import checkPropTypes from 'check-prop-types';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
@@ -38,6 +37,5 @@ it('renders non-empty congrats message when `success` is true', () => {
 
 it('does not throw warning with expected props', () => {
   const expectedProps = { success: false };
-  const propError = checkPropTypes(Congrats.propTypes, expectedProps, 'prop', Congrats.name);
-  expect(propError).toBeUndefined();
+  checkProps(Congrats, expectedProps)
 })
