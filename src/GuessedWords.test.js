b3f24fd31a22d71a874585d11311ad3ec820a337
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-14 11:55:37 +0100

    chore: add semicolon to import statement

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../test/testUtils';
 
-import GuessedWords from './GuessedWords'
+import GuessedWords from './GuessedWords';
 
 const defaultProps = {
   guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
