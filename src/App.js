1e8285c8cf238a106adc878b60b649f0d303b0e1
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-14 10:22:36 +0100

    feat: import components and display them in app component, add container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
 
 class App extends Component {
   constructor(props) {
@@ -13,10 +15,15 @@ class App extends Component {
   render() {
 
     return (
-      <div data-test="app-component"></div>
+      <div className='container'>
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[ 
+          {guessedWord: 'train', letterMatchCount: 3 }
+        ]} /> 
+      </div>
     );
   };
 }
-
 
 export default App;
