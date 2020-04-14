08d09c6b21e9ef55df1ed9e39475b8ec9fb423c5
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-14 11:16:21 +0100

    chore: clean up unneeded imports and configuration now handled by setupTests.js and testUtils.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { findByTestAttr } from '../test/testUtils';
 
 import App from './App';
-
-Enzyme.configure({ adapter: new EnzymeAdapter()})
 
 const setup = (props={}, state=null) => {
   const wrapper = shallow(<App {...props} />)
   if (state) wrapper.setState(state)
     return wrapper;
-}
-
-const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test="${val}"]`);
 }
 
 test('renders without error', () => {
