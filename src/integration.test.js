52f89379af6f3a9d3c89001540c5f60b9af6678f
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-23 16:03:51 +0100

    test: create and test guessedWordsReducer

---
+++ src/integration.test.js
@@ -0,0 +1,24 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('updates state correctly for an unsuccessful guess', () => {
+
+    })
+
+    test('updates state correctly for a successful guess', () => {
+
+    })
+  })
+
+  describe('some guessed words', () => {
+    test('updates state correctly for an unsuccessful guess', () => {
+
+    })
+
+    test('updates state correctly for a successful guess', () => {
+
+    })
+  })
+})
