0065a59ed73b9be23f7688bb4a60a73c644dc8d8
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-23 16:15:08 +0100

    test: create ssecret word reducer

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,9 +2,27 @@ import { storeFactory } from '../test/testUtils';
 import { guessWord } from './actions';
 
 describe('guessWord action dispatcher', () => {
-  describe('no guessed words', () => {
-    test('updates state correctly for an unsuccessful guess', () => {
+  const secretWord = 'party'
+  const unsuccessfulGuess = 'train';
 
+  describe('no guessed words', () => {
+    let store
+    const initialState = { secretWord}
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    })
+    test('updates state correctly for an unsuccessful guess', () => {
+      store.dispatch(guessWord(unsuccesfulGuess));
+      const newState = store.getState()
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [{
+          guessedWWord: unsuccessfulGuess,
+          letterMatchCount: 3
+        }]
+      }
+      expect(newState).toEqual(expectedState)
     })
 
     test('updates state correctly for a successful guess', () => {
