181db890dbac6ce98fd10bab7ffdf4a443be6d2b
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-23 11:30:17 +0100

    chore: move components into a component folder

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme'
 
-import { findByTestAttr, storeFactory } from '../test/testUtils';
+import { findByTestAttr, storeFactory } from '../../test/testUtils';
 import Input from './Input'
 
 const setup = (initialState={}) => {
@@ -61,8 +61,8 @@ describe('render', () => {
   })
 })
 
-describe('update state', () => {
+// describe('update state', () => {
 
-  test('', () => {})
+//   test('', () => {})
   
-})
+// })
